11dc2efc93b427fba27b31d151a0634c0964dd79
Stefan Petrovic <petroste@Stefans-MacBook-Pro.local>
2023-02-04 12:41:28 -0500

    visual and login changes

--- client/helpdesk/src/App.js
+++ client/helpdesk/src/App.js
@@ -2,6 +2,8 @@ import Navbar from './components/index.js';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home.js';
 import About from './pages/about.js';
+import Login from './pages/login.js';
+import './App.css';
   
 function App() {
     return (
@@ -11,6 +13,7 @@ function App() {
             <Routes>
               <Route path='/' exact element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
             </Routes>
           </BrowserRouter>
         </div>
