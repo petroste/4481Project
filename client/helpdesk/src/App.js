8afbea0d95eaa582f5041d478c186e8abfba284c
Stefan Petrovic <petroste@Stefans-MacBook-Pro.local>
2023-02-03 19:03:46 -0500

    new client base

--- client/helpdesk/src/App.js
+++ client/helpdesk/src/App.js
@@ -1,11 +1,30 @@
-import socketIO from 'socket.io-client';
-const socket = socketIO.connect('http://localhost:4000');
-
+import logo from "./logo.svg";
+import "./App.css";
+  
 function App() {
-    return (
-        <div>
-            <p>Hello World</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" 
+             alt="logo" />
+          
+<p>A simple React app.....</p>
+  
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <form action="../../post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
+      </header>
+    </div>
+  );
 }
+  
 export default App;
