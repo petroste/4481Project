a50b67656d34b69d0e40f7b5e68c9ab618c43aa0
Stefan Petrovic <petroste@Stefans-MacBook-Pro.local>
2023-02-08 16:26:08 -0500

    intermediary fix

--- client/helpdesk/src/App.js
+++ client/helpdesk/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import Navbar from './components/index.js';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home.js';
