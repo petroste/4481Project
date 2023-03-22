4fff3c1054cf1cee25bb737089b1bcfba7a0d8c7
Dexuan Ren <81537377+KenRen98@users.noreply.github.com>
2023-03-22 19:28:32 -0400

    Added input validation using express-validator, to avoid injection.
    Need further testing to ensure function compatibility or dynamic errors.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,39 +1,54 @@
 const config = require("../config/auth.config");
 const db = require("../models");
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 const User = db.users;
 
-exports.login = (req, res) => {
-    User.findOne({username: req.body.username})
-        .exec((err, user) => {
-            if (err)
-            {
-                res.status(500).send({message: err});
-                return;
-            }
-            if (!user)
-            {
-                return res.status(404).send({message: "User does not exist"});
-            }
+exports.login = [
+  // input validation using express-validator
+  body("username").notEmpty().trim().escape(),
+  body("password").notEmpty().trim(),
 
-            var isPwCorrect = bcrypt.compareSync(req.body.password, user.password);
+  (req, res) => {
+    // check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-            if (!isPwCorrect)
-            {
-                return res.status(401).send({
-                    accessToken: null,
-                    message: "Invalid password!"
-                });
-            }
+    // retrieve user from the database using a parameterized query with prepared statements
+    User.findOne({ username: req.body.username })
+      .select("+password")
+      .exec((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        if (!user) {
+          return res.status(404).send({ message: "User does not exist" });
+        }
 
-            var token = jwt.sign({id: user.id}, config.secret, {expiresIn: 86400});
+        // validate password
+        const isPwCorrect = bcrypt.compareSync(req.body.password, user.password);
 
-            res.status(200).send({
-                id: user._id,
-                username: user.username,
-                accessToken: token
-            });
+        if (!isPwCorrect) {
+          return res.status(401).send({
+            accessToken: null,
+            message: "Invalid password!",
+          });
+        }
+
+        // generate JWT token
+        const token = jwt.sign({ id: user.id }, config.secret, {
+          expiresIn: 86400,
         });
-};
 
+        res.status(200).send({
+          id: user._id,
+          username: user.username,
+          accessToken: token,
+        });
+      });
+  },
+];
